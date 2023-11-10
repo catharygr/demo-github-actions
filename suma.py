50662a942ef395884d9fff24eabdea75ec1d9375
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-10 16:07:19 +0100

    Prueba de CICD-2

--- suma.py
+++ suma.py
@@ -1,5 +1,26 @@
 def sumar(a, b):
     return a + b
 
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir por cero")
+    return a / b
+
 if __name__ == "__main__":
     print(sumar(1, 2))
+    print(restar(1, 2))
+    print(multiplicar(1, 2))
+    try:
+       print(dividir(5, 2))
+       print(dividir(1, 0))
+    except ValueError as e:
+        print(e)
+
+
+
--- test_suma.py
+++ test_suma.py
@@ -1,5 +1,6 @@
 import unittest
-from suma import sumar
+from suma import sumar, restar, multiplicar, dividir
+
 
 class TestSumar(unittest.TestCase):
 
@@ -7,6 +8,22 @@ class TestSumar(unittest.TestCase):
     self.assertEqual(sumar(3, 2), 5)
     self.assertEqual(sumar(-1, 1), 0)
     self.assertEqual(sumar(-1, -1), -2)
+
+  def test_restar(self):
+    self.assertEqual(restar(3, 2), 1)
+    self.assertEqual(restar(-1, 1), -2)
+    self.assertEqual(restar(-1, -1), 0)
+
+  def test_multiplicar(self):
+    self.assertEqual(multiplicar(3, 2), 6)
+    self.assertEqual(multiplicar(-1, 1), -1)
+    self.assertEqual(multiplicar(-1, -1), 1)
+
+  def test_dividir(self):
+    self.assertEqual(dividir(3, 2), 1.5)
+    self.assertEqual(dividir(-1, 1), -1)
+    self.assertEqual(dividir(-1, -1), 1)
+    self.assertRaises(ValueError, dividir, 1, 0)
 
 if __name__ == '__main__':
     unittest.main()
